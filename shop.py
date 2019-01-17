eaadfcb395890b495c66c4023d4ede2fd2393c68
PocketEngi <2997583+PocketEngi@users.noreply.github.com>
2019-01-16 23:02:30 -0500

    Refectored "database" to be easier to work with

--- shop.py
+++ shop.py
@@ -3,30 +3,30 @@
 from decimal import *
 
 # this would be in a database
-fake_db = {
-    'hen': {
+fake_db = [
+    {
         'title': "French hen",
         'price': Decimal('10.17'),
         'inventory_count': 3
     },
-    'dove': {
+    {
         'title': "Turtle dove",
         'price': Decimal('12.25'),
         'inventory_count': 2
     },
-    'partridge': {
+    {
         'title': "Partridge in a pear tree",
         'price': Decimal('9.99'),
         'inventory_count': 1
     },
-    'chachalaca': {
+    {
         'title': "Chachalaca",
         'price': Decimal('3.50'),
         'inventory_count': 0
     }
-}
+]
 
-# may be changed to set up for unit tests
+# may be changed externally to set up for unit tests
 ITEMS = fake_db
 
 
@@ -39,6 +39,6 @@ def list_items(in_stock_only=False):
     :type in_stock_only: bool
     :return: List of dicts. All products, sorted by title.
     """
-    return sorted([ITEMS[k] for k in ITEMS.keys()
-                   if (ITEMS[k]['inventory_count'] > 0 if in_stock_only else True)],
+    return sorted([k for k in ITEMS
+                   if (k['inventory_count'] > 0 if in_stock_only else True)],
                   key=lambda i: i['title'])
--- test_shop.py
+++ test_shop.py
@@ -3,23 +3,23 @@
 from decimal import *
 import shop
 
-ITEM_TEST_1 = {
-    'dove': {
+ITEM_TEST_1 = [
+    {
         'title': "Turtle dove",
         'price': Decimal('12.25'),
         'inventory_count': 2
     },
-    'partridge': {
+    {
         'title': "Partridge in a pear tree",
         'price': Decimal('9.99'),
         'inventory_count': 1
     },
-    'chachalaca': {
+    {
         'title': "Chachalaca",
         'price': Decimal('3.50'),
         'inventory_count': 0
     }
-}
+]
 
 
 class TestListItems:
@@ -64,4 +64,4 @@ class TestListItems:
 
     def test_request_in_stock_empty_store(self):
         shop.ITEMS = dict()
-        assert shop.list_items() == []
+        assert shop.list_items(in_stock_only=True) == []
